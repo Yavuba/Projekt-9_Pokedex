6886f675283962ff6ca53ad7ed3c810e616ab3bd
Sefa <u4877157914@gmail.com>
2026-05-08 20:59:25 +0200

    update modal data

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // variables //
 let pokeapiArray = [];
 let pokeTypesArray = [];
+
+let pokeMainCache = {};
+let pokeSpeciesCache = {};
+let pokeEvoCache = {};
+let currentSpecies = null;
+
 let currentIndex = 0;
-let modal = document.getElementById("myModal");
-let modalImg = document.getElementById("img");
-let captionText = document.getElementById("caption");
+let modal = document.getElementById("modal");
 
 // init function for calling api-data //
 async function init() {
@@ -53,7 +57,7 @@ async function getUrlData() {
 
 // get Pokemon main-data //
 async function getPokemonMainData() {
-  let contentRef = document.getElementById('contentRow');
+  let contentRef = document.getElementById('content');
   contentRef.innerHTML = "";
 
   for (const pokemonUrl of pokeapiArray) {
@@ -65,6 +69,7 @@ async function getPokemonMainData() {
         }
 
         const result = await response.json();
+        pokeMainCache[result.id] = result;
         renderPokemonList(result);
         
     } catch (error) {
@@ -73,57 +78,92 @@ async function getPokemonMainData() {
   }
 }
 
+// fetch data for species //
+async function getSpeciesData(id) {
+  if (pokeSpeciesCache[id]) {
+      return pokeSpeciesCache[id];
+    }
+
+  const url = pokeMainCache[id].species.url;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    pokeSpeciesCache[id] = data;
+    return data;
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// fetch data for evo chain //
+async function getEvoData(id) {
+  if (pokeEvoCache[id]) {
+      return pokeEvoCache[id];
+    }
+
+  const url = pokeSpeciesCache[id].evolution_chain.url;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    pokeEvoCache[id] = data;
+    return data;
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 // render-function to create pokemon-list in HTML //
 function renderPokemonList(result) {
-    let contentRef = document.getElementById('contentRow');
+    let contentRef = document.getElementById('content');
     contentRef.innerHTML += getPokemonListTemplate(result);
 }
 
 // get all type-information for pokemon //
 function getPokemonTypes(pokemon) {
   let text = "";
-  for (let index = 0; index < pokemon.types.length; index++) {
-      text += `<li id="type-${pokemon.id}-${index}" class="${pokemon.types[index].type.name}">${pokemon.types[index].type.name}</li>`;
+  for (let i = 0; i < pokemon.types.length; i++) {
+      text += getPokemonTypesTemplate(pokemon, i);
   }
   return text;
 }
 
-// Modal functions //
-// defining current index i with clicked modal // 
-function openModal(i) {
-  currentIndex = i;
-  getModalByIndex(currentIndex);
+// modal functions //
+
+// get data and open modal // 
+async function getDataModal(id) {
+  currentIndex = id;
+  let contentRef = document.getElementById('modal');
+  let data = pokeMainCache[id];
+
+  contentRef.innerHTML = getModalTemplate(data);
+  modal.showModal();
+
+  currentSpecies = await getSpeciesData(id);
+  currentEvo = await getEvoData(id);
+  renderData(id, 'about');
 }
 
-// get modal information with current index //
-function getModalByIndex(index) {
-  modal.showModal(index);
-  modalImg.src = `./img/gallery/monkeys/${images[index]}`;
-  modalImg.alt = `Monkey image ${index + 1}`;
-  captionText.innerHTML = images[index].split('.').slice(0, -1).join('.');
-  photoNr(index);
-  getFavoriteIcon(index);
-}
-
-// change modal-window, prev or next (1 or -1) //
-function renderFiltered(direction) {
-  currentIndex += direction;
-  if (currentIndex < 0) {
-      currentIndex = images.length - 1;
-  }
-
-  if (currentIndex >= images.length) {
-      currentIndex = 0;
-  }
-  getModalByIndex(currentIndex);
-}
 
 // closing modal //
 function closeModal() {
   modal.close();
 }
 
-// moving to previous or next gallery-pictures with arrow-keys + activate/deactivate favorite//
+// moving to previous or next pokemon with arrow-keys //
 function modalKeys(event) {
   if (event.key === "ArrowLeft") {
       event.preventDefault();     
@@ -136,3 +176,123 @@ function modalKeys(event) {
       return;
   }
 }
+
+// change modal-window, prev or next (1 or -1) //
+function renderFiltered(direction) {
+  currentIndex += direction;
+  const total = Object.keys(pokeMainCache).length
+
+  if (currentIndex < 1) {
+      currentIndex = total;
+  }
+
+  if (currentIndex > total) {
+      currentIndex = 1;
+  }
+  getDataModal(currentIndex);
+}
+
+// function for rendering overlay //
+function renderData(id, type, species) {
+  const contentRef = document.getElementById('menu-content-' + id)
+  const data = pokeMainCache[id];
+
+  const templates = {
+    about: () => getAboutTemplate(data, currentSpecies),
+    stats: () => getStatsTemplate(data),
+    moves: () => getMovesTemplate(data),
+    evo: () => getEvoTemplate(data, currentEvo)
+  };
+  const template = templates[type];
+  contentRef.innerHTML = template();
+}
+
+// get class for formatting buttons background //
+function getBackgroundBtn(id, btn) {
+  const buttons = ['about', 'stats', 'moves', 'evo'];
+
+  buttons.forEach(name => { const element = document.getElementById(`button-${name}-${id}`);
+  element.classList.remove("class-background");
+  });
+
+  btn.classList.add("class-background");
+}
+
+// functions for formatting/getting pokemon data - ABOUT //
+
+function convertHeight(value) {
+  const totalInches = Math.round(value * 3.937);
+  const feet = Math.floor(totalInches / 12);
+  const inches = totalInches % 12;
+  const result = "("+(value * 10)+ " cm)";
+
+  return `${feet}'${inches}" ${result}`;
+}
+
+function convertWeight(value) {
+  const kg = (value / 10).toFixed(1);
+  const lbs = (kg * 2.20462).toFixed(1);
+  
+  return `${lbs} lbs (${kg} kg)`;
+}
+
+function getAbilities(pokemon) {
+  return pokemon.abilities
+  .map(a => a.ability.name)
+  .join(", ");
+}
+
+function getSpecies(species) {
+  return species.genera.find(g => g.language.name === "en").genus;
+}
+
+function getGender(species) {
+  const rate = species.gender_rate;
+
+  if (rate === -1) return "Genderless";
+
+  const female = (rate / 8) * 100;
+  const male = 100 - female;
+
+  return `${male}% ♂ / ${female}% ♀`;
+}
+
+function getEggGroups(species) {
+  return species.egg_groups.map(g => g.name).join(", ");
+}
+
+function getEggCycle(species) {
+  const steps = species.hatch_counter * 255;
+  return `${species.hatch_counter} (${steps.toLocaleString('de-DE')} steps)`;
+}
+
+// functions for formatting/getting pokemon data - STATS //
+
+function getPercStat(value) {
+  const percent = (value / 255) * 100;
+
+  return percent;
+}
+
+function getSumBst(pokemon) {
+  let total = 0;
+
+  for (const stat of pokemon.stats) {
+    total += stat.base_stat;
+  }
+  return total;
+}
+
+function getPercBst(total) {
+  const percent = (total / 720) * 100;
+
+  return percent;
+}
+
+// functions for formatting/getting pokemon data - EVO CHAIN //
+
+function getEvoChain(evo) {
+  return pokeEvoCache[1].chain.evolves_to[0].evolves_to[0].species.name;
+  return pokeEvoCache[1].chain.evolves_to[0].species.name;
+}
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,7 @@
 // creating list of pokemons in main //
 function getPokemonListTemplate(result) {
     return `
-            <button id="pokemon-${result.id}" class="${result.types[0].type.name} section-pokemon" onclick="openModal(${result.id})">
+            <button id="pokemon-${result.id}" class="${result.types[0].type.name} section-pokemon" onclick="getDataModal(${result.id})">
                 <div id="card-layout-${result.id}" class="layout-card">
                     <figure id="figure-${result.id}">
                         <img id="img-${result.id}" class="pokemon-img" src="${result.sprites.other["official-artwork"].front_default}" alt="pokemon-img">
@@ -14,31 +14,137 @@ function getPokemonListTemplate(result) {
             `
 }
 
-// rendering modal-window for each pokemon //
-function openModal(id) {
+// rendering pokemon types //
+function getPokemonTypesTemplate(pokemon, index) {
     return `
-            <dialog id="myModal-${id}" class="modal" tabindex="0" aria-labelledby="caption" onclick="closeModal()" onkeydown="modalKeys(event)">
-                <section id="header-modal-${id}" class="header-modal" onclick="event.stopPropagation()"> 
-                    <button id="close-button-icon-${id}" class="close-icon" aria-label="closing dialog" onclick="closeModal()"></button>
-                    <h2 id="caption-pokemon-${id}" class="title-modal"></h2>
-                    <span id="modal-id-${id}" class="id"></span>
-                    <div id="type-modal-${id}" class="type"></div>
-                </section>
-                <img id="modal-img-${id}" class="modal-pokemon-img" onclick="event.stopPropagation()">
-                <section id="modal-content-${id}" class="content-modal" onclick="event.stopPropagation()">
-                    <div id="pokemon-modal-menu-${id}" class="pokemon-menu">
-                        <Button id="button-about-${id}">About</Button>
-                        <Button id="button-stats-${id}">Stats</Button>
-                        <Button id="button-moves-${id}">Moves</Button>
-                        <Button id="button-evo-${id}">Evo Chain</Button>
-                    </div>
-                    <div id="menu-content-${id}" class="pokemon-data"></div>
-                    <div id="change-pokemon-${id}"
-                        <button id="prev-${id}" class="left" aria-label="go to previous gallery picture" onclick="renderFiltered(-1)"></button> 
-                        <button id="next-${id}" class="right" aria-label="go to next gallery picture" onclick="renderFiltered(1)"></button>
-                    </div>
-                </section>
-            </dialog>
+            <li id="type-${pokemon.id}-${index}" class="${pokemon.types[index].type.name}">${pokemon.types[index].type.name}
+            </li>
             `
 }
 
+// rendering modal-window for each pokemon //
+function getModalTemplate(result) {
+    return `
+            <div id="modal-pokemon-${result.id}" class="modal-class ${result.types[0].type.name}">
+                <section id="header-modal-${result.id}" class="header-modal" onclick="event.stopPropagation()"> 
+                    <button id="close-button-icon-${result.id}" class="close-icon" aria-label="closing dialog" onclick="closeModal()"></button>
+                    <h2 id="caption-pokemon-${result.id}" class="name name-modal">${result.name}</h2>
+                    <span id="modal-id-${result.id}" class="id id-modal">#${result.id}</span>
+                    <figure id="figure-modal-${result.id}" class="div-figure">
+                        <figcaption id="type-modal-${result.id}" class="type type-modal">${getPokemonTypes(result)}</figcaption>
+                        <img id="modal-img-${result.id}" class="pokemon-img modal-img" src="${result.sprites.other["official-artwork"].front_default}" alt="pokemon-img" onclick="event.stopPropagation()">
+                    </figure>
+                </section>
+                <section id="modal-content-${result.id}" class="content-modal" onclick="event.stopPropagation()">
+                    <div id="pokemon-modal-menu-${result.id}" class="pokemon-menu">
+                        <button id="button-about-${result.id}" class="class-background" onclick="renderData(${result.id}, 'about'); getBackgroundBtn(${result.id}, this);">About</Button>
+                        <button id="button-stats-${result.id}" onclick="renderData(${result.id}, 'stats'); getBackgroundBtn(${result.id}, this);">Stats</Button>
+                        <button id="button-moves-${result.id}" onclick="renderData(${result.id}, 'moves'); getBackgroundBtn(${result.id}, this);">Moves</Button>
+                        <button id="button-evo-${result.id}" onclick="renderData(${result.id}, 'evo'); getBackgroundBtn(${result.id}, this);">Evo Chain</Button>
+                    </div>
+                    <div id="menu-content-${result.id}" class="pokemon-data">
+
+                    </div>
+                    <div id="change-pokemon-${result.id}" class="change-pokemon">
+                        <button id="prev-${result.id}" class="left" aria-label="go to previous gallery picture" onclick="renderFiltered(-1)"></button> 
+                        <button id="next-${result.id}" class="right" aria-label="go to next gallery picture" onclick="renderFiltered(1)"></button>
+                    </div>
+                </section>
+            </div>
+            `
+}
+
+// rendering template for "About" Data //
+function getAboutTemplate(result, species) {
+    return `
+            <table id="data-about-${result.id}" class="table">
+                    <tr>
+                        <td style="width: 27%;"><strong>Species</strong></td>
+                        <td style="width: 73%;">${getSpecies(species)}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Height</strong></td>
+                        <td>${convertHeight(result.height)}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Weight</strong></td>
+                        <td>${convertWeight(result.weight)}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Abilities</strong></td>
+                        <td class="class-capitalize">${getAbilities(result)}</td>
+                    </tr>
+                    <tr>
+                        <td class="breeding-class">Breeding</td>
+                    </tr>
+                    <tr>
+                        <td class="gender-class">Gender</td>
+                        <td class="gender-class">${getGender(species)}</td>
+                    </tr>
+                    <tr>
+                        <td>Egg Groups</td>
+                        <td class="class-capitalize">${getEggGroups(species)}</td>
+                    </tr>
+                    <tr>
+                        <td>Egg Cycle</td>
+                        <td>${getEggCycle(species)}</td>
+                    </tr>
+            </table>
+            `
+}
+
+// rendering template for "Stats" Data //
+function getStatsTemplate(result) {
+    return `
+            <div class="stats-bar">
+                ${result.stats.map(s => `
+                    <div class="light-grey">
+                        <span class="name-stats">${s.stat.name}</span>
+                        <div class="bar-wrapper">
+                            <div class="container ${result.types[0].type.name}" style="width:${getPercStat(s.base_stat)}%">
+                                <span class="stat">${s.base_stat}</span>
+                            </div>
+                        </div>
+                    </div>
+                `).join('')}
+                <div class="light-grey stat-total">
+                        <span class="name-stats"><strong>Base Stat Total</strong></span>
+                        <div class="bar-wrapper">
+                            <div class="container ${result.types[0].type.name}" style="width:${getPercBst(getSumBst(result))}%">
+                                <span class="stat">${getSumBst(result)}</span>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            `
+}
+
+// rendering template for "Moves" Data //
+function getMovesTemplate(result) {
+    return `
+            <div id="data-moves-${result.id}" class="table">
+                    <div class="title-move">
+                        <div style="width: 27%;"><strong>Moves</strong></div>
+                        <div style="width: 73%;"></div>
+                    </div>
+                    <div class="moves-wrapper">
+                        ${result.moves
+                            .slice(0, 20)
+                            .map(m => `
+                            <div class="moves">
+                                ${m.move.name}
+                            </div>
+                        `).join('')}    
+                    </div>
+            </div>
+            `
+}
+
+// rendering template for "Evo Chain" Data //
+function getEvoTemplate(result, evo) {
+    return `
+            <div>${getEvoChain(evo)}</div>
+
+
+            `
+}
